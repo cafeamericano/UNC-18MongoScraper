962d25fda079b43847339b582e0d2da8426a1c06
Matthew <mfarmer5102@gmail.com>
2019-09-10 23:10:49 -0400

    Implemented testing routes for adding articles and comments

--- server.js
+++ server.js
@@ -17,13 +17,43 @@ app.use(express.static("public"));
 var db = require("./models");
 
 //Connect to database
-mongoose.connect("mongodb://localhost/unit18Populater", {
+mongoose.connect("mongodb://localhost/week18homework", {
   useNewUrlParser: true
 });
 
 //Routes
 app.get("/", function(req, res) {
   res.send("hit home route");
+});
+
+app.get("/addanarticle", function(req, res) {
+  db.Article.create({
+    headline: "Apple",
+    summary: "It's a red fruit",
+    url: "Too sugary."
+  })
+    .then(function(dbArticle) {
+      console.log(dbArticle);
+      res.send("Added article.");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
+
+app.get("/addacomment", function(req, res) {
+  db.Comment.create({
+    user: "johnsmith",
+    commentText: "No way.",
+    associatedArticleURL: "Too sugary."
+  })
+    .then(function(dbComment) {
+      console.log(dbComment);
+      res.send("Added comment.");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
 
 //Start server
